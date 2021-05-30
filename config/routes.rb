e9b4f5ff6c5133481e50b4207fe4064a3fe12321
Kowa Jia Liang <jlkowa1229@gmail.com>
2021-05-30 20:40:56 +0800

    Added some static pages and modeled user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+
+  get 'signup' => 'users#new'
+
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
